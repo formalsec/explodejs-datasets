34461abde1c9710d11f733a0b1d0275841e5ad4f
Mafalda Ferreira <mafalda.baptista@tecnico.ulisboa.pt>
2024-05-13 17:44:03 +0100

    Add preliminary results from new VIP (CWE 78)

--- vulcan-dataset/CWE-78/356/src/stats.js
+++ vulcan-dataset/CWE-78/356/src/stats.js
@@ -93,26 +93,6 @@ var stats = {
    * on linux it could be --no-header
    * on solaris 11 can't figure out a way to do this properly so...
    */
-  ps: function(pid, options, done) {
-
-    var cmd = 'ps -o pcpu,rss -p '
-
-    if(os.platform() == 'aix')
-      cmd = 'ps -o pcpu,rssize -p ' //this one could work on other platforms
-
-    exec(cmd + pid, function(err, stdout, stderr) {
-      if(err)
-        return done(err, null)
-
-      stdout = stdout.split(os.EOL)[1]
-      stdout = stdout.replace(/^\s+/, '').replace(/\s\s+/g, ' ').split(' ')
-
-      return done(null, {
-        cpu: parseFloat(stdout[0].replace(',', '.')),
-        memory: parseFloat(stdout[1]) * 1024
-      })
-    })
-  },
   /**
    * This is really in a beta stage
    */
@@ -177,7 +157,28 @@ var stats = {
       return done(null, {cpu: (total / seconds) * 100, memory: workingsetsize})
     })
     wmic.stdin.end()
+  },
+    ps: function(pid, options, done) {
+
+    var cmd = 'ps -o pcpu,rss -p '
+
+    if(os.platform() == 'aix')
+      cmd = 'ps -o pcpu,rssize -p ' //this one could work on other platforms
+
+    exec(cmd + pid, function(err, stdout, stderr) {
+      if(err)
+        return done(err, null)
+
+      stdout = stdout.split(os.EOL)[1]
+      stdout = stdout.replace(/^\s+/, '').replace(/\s\s+/g, ' ').split(' ')
+
+      return done(null, {
+        cpu: parseFloat(stdout[0].replace(',', '.')),
+        memory: parseFloat(stdout[1]) * 1024
+      })
+    })
   }
+
 }
 
 module.exports = stats;
